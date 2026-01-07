807c2710508a307ae1b6ac6b42a1a05ff27a1dff
Owais101-afk <owaisali753@gmail.com>
2026-01-07 22:34:56 +0530

    second

--- Desktop/js project 1 SNAKE GAME/script.js
+++ Desktop/js project 1 SNAKE GAME/script.js
@@ -4,6 +4,8 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId = null;
+let snakeFood = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
 
 const blocks = [];
 const snake = [
@@ -28,18 +30,11 @@ for (let row = 0; row < rows; row++) {
 
 function renderSnake() {
 
-    snake.forEach((block) => {
-        blocks[`${block.x},${block.y}`].classList.add("fill")
-    })
+    let head = null;
+    // console.log(head);
 
-}
+    blocks[`${snakeFood.x},${snakeFood.y}`].classList.add("snakeFood")
 
-renderSnake()
-
-
-setInterval(() => {
-
-    let head = {};
     if (direction === "left") {
         head = { x: snake[0].x, y: snake[0].y - 1 }
     } else if (direction === "right") {
@@ -50,17 +45,39 @@ setInterval(() => {
         head = { x: snake[0].x + 1, y: snake[0].y }
     }
 
+    if (head.x == snakeFood.x && head.y == snakeFood.y) {
+        blocks[`${snakeFood.x},${snakeFood.y}`].classList.remove("snakeFood");
+        snakeFood = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+        blocks[`${snakeFood.x},${snakeFood.y}`].classList.add("snakeFood");
+        snake.unshift(head);
+
+    }
+
+    if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+        alert("game over");
+        clearInterval(intervalId);
+
+    }
+
     snake.forEach((block) => {
-
         blocks[`${block.x},${block.y}`].classList.remove("fill")
-
     })
 
-    snake.unshift(head)
-        snake.pop();
-    
+    snake.unshift(head);
+    console.log(head);
+
+    snake.pop();
+    snake.forEach(segment => {
+        blocks[`${segment.x},${segment.y}`].classList.add("fill")
+    })
+
+}
+
+
+intervalId = setInterval(() => {
     renderSnake();
-}, 500)
+}, 300)
+
 
 addEventListener('keydown', (event) => {
 
